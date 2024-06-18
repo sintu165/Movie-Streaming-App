a5c60ee0a022b738a566694c7d2a9e177c1d9330
Brahma Swaroop <brahmaswaroop1999@gmail.com>
2024-06-18 19:27:30 +0530

    In this onclick listener and exoplayer is implemented.App almost completed.

--- app/src/main/java/com/example/moviestreamingapp/MainActivity.java
+++ app/src/main/java/com/example/moviestreamingapp/MainActivity.java
@@ -112,19 +112,58 @@ public class MainActivity extends AppCompatActivity {
         HomeCateegoryItem3.add(new ChildRecyclerViewDataClass(4,"12th Fail","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/kids%20and%20family%20movies%2F12thfail.jpg?alt=media&token=22c6bd7e-2f5d-4c36-92da-d6e37c1a9e4d",""));
         HomeCateegoryItem3.add(new ChildRecyclerViewDataClass(5,"House the Dragon","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/kids%20and%20family%20movies%2Fhouse%20of%20the%20%20dragon.jpg?alt=media&token=bc6c5b47-3f86-4ec3-9aae-48b7c5c4d3dd",""));
 
-        //Up Coming Movies list
+        //Romantic Movies
         List<ChildRecyclerViewDataClass>HomeCateegoryItem4 =new ArrayList<>();
-        HomeCateegoryItem4.add(new ChildRecyclerViewDataClass(1,"Dealpool vs Wolverine","https://wallpapercave.com/wp/wp4457490.jpg",""));
-        HomeCateegoryItem4.add(new ChildRecyclerViewDataClass(2,"charnobyl","https://wallpapercave.com/wp/wp4457490.jpg",""));
-        HomeCateegoryItem4.add(new ChildRecyclerViewDataClass(3,"charnobyl","https://wallpapercave.com/wp/wp4457490.jpg",""));
-        HomeCateegoryItem4.add(new ChildRecyclerViewDataClass(4,"charnobyl","https://wallpapercave.com/wp/wp4457490.jpg",""));
-        HomeCateegoryItem4.add(new ChildRecyclerViewDataClass(5,"charnobyl","https://wallpapercave.com/wp/wp4457490.jpg",""));
+        HomeCateegoryItem4.add(new ChildRecyclerViewDataClass(1,"The Proposal","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/romantic%20movies%2Fthe%20proposal.jpg?alt=media&token=769bf811-62e0-4d26-894a-ad3ed5cdbe3d",""));
+        HomeCateegoryItem4.add(new ChildRecyclerViewDataClass(2,"La La Land","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/romantic%20movies%2Fla-la-land.jpg?alt=media&token=d9ca0412-1502-4a5f-9cef-ce3d9beb7d80",""));
+        HomeCateegoryItem4.add(new ChildRecyclerViewDataClass(3,"Meet Joe Black","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/romantic%20movies%2FMeet%20joe%20Black.jpg?alt=media&token=9fc8eb0b-e3e6-4229-84b0-202132c4891e",""));
+        HomeCateegoryItem4.add(new ChildRecyclerViewDataClass(4,"The NoteBook","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/romantic%20movies%2FThe%20NoteBook.jpg?alt=media&token=b219e901-e79e-4f4e-9d06-071a5e1d254e",""));
+        HomeCateegoryItem4.add(new ChildRecyclerViewDataClass(5,"Titanic","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/romantic%20movies%2Ftitanic.jpg?alt=media&token=46d01707-f993-45ce-8b4e-02432cafd15f",""));
+
+        //Action and Thriller
+        List<ChildRecyclerViewDataClass>HomeCateegoryItem5 =new ArrayList<>();
+        HomeCateegoryItem5.add(new ChildRecyclerViewDataClass(1,"Mad Max:Fury Road","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Action%20and%20Thriller%20Movies%2FMad%20Max%20Fury%20Road.jpg?alt=media&token=f2917557-c347-4dfd-8540-148cc1687973",""));
+        HomeCateegoryItem5.add(new ChildRecyclerViewDataClass(2,"No Time To Die","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Action%20and%20Thriller%20Movies%2FNo%20time%20to%20die.jpeg?alt=media&token=1c83142c-e740-49cc-b7fa-40c0462dcaea",""));
+        HomeCateegoryItem5.add(new ChildRecyclerViewDataClass(3,"American Sniper","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Action%20and%20Thriller%20Movies%2Famerican%20sniper.jpg?alt=media&token=f66ed644-5ec0-4aaf-bcad-fed328390410",""));
+        HomeCateegoryItem5.add(new ChildRecyclerViewDataClass(4,"Inception","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Action%20and%20Thriller%20Movies%2Finception.jpg?alt=media&token=7ce209b9-a606-48c9-9595-6bd4f2ab6f16",""));
+        HomeCateegoryItem5.add(new ChildRecyclerViewDataClass(5,"John Wick 4","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Action%20and%20Thriller%20Movies%2Fjohn%20wick%204.jpeg?alt=media&token=0e7e54eb-1801-4b0e-ba1a-708e58eb31fe",""));
+
+        //Top-rated IMDb movies
+        List<ChildRecyclerViewDataClass>HomeCateegoryItem6 =new ArrayList<>();
+        HomeCateegoryItem6.add(new ChildRecyclerViewDataClass(1,"The Godfather","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Top-rated%20IMDb%2FThe%20godfather.jpg?alt=media&token=0eb53ca2-e2dc-4672-ae32-72d7577ec281",""));
+        HomeCateegoryItem6.add(new ChildRecyclerViewDataClass(2,"The Load of the ring:The return of the King","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Top-rated%20IMDb%2FThe%20load%20of%20the%20ring%20the%20return%20of%20the%20king.jpg?alt=media&token=2c22643f-0fff-4639-ac74-86c491b7a62c",""));
+        HomeCateegoryItem6.add(new ChildRecyclerViewDataClass(3,"Forrest-Gump","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Top-rated%20IMDb%2Fforrest-gump.jpg?alt=media&token=6a074fa7-44d2-4132-804c-3a46dd6f7e85",""));
+        HomeCateegoryItem6.add(new ChildRecyclerViewDataClass(4,"Goodfellas","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Top-rated%20IMDb%2Fgoodfellas.jpg?alt=media&token=da16502a-f4d3-4967-825a-80b413b300c7",""));
+        HomeCateegoryItem6.add(new ChildRecyclerViewDataClass(5,"Matrix","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Top-rated%20IMDb%2Fmatrix.jpg?alt=media&token=c27af84a-2acd-456b-87cc-163cb555c670",""));
+
+        //Documentries movies
+        List<ChildRecyclerViewDataClass>HomeCateegoryItem7 =new ArrayList<>();
+        HomeCateegoryItem7.add(new ChildRecyclerViewDataClass(1,"Schindler's  list","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Documentry%20%20movies%2Fschindler's%20list.jpeg?alt=media&token=4e0bae84-8463-48ef-bc75-5a59595539ce",""));
+        HomeCateegoryItem7.add(new ChildRecyclerViewDataClass(2,"Pearl harbor","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Documentry%20%20movies%2Fpearl%20harbor.jpg?alt=media&token=aa4647a5-7d69-4c8d-8d35-4dc847c03ba8",""));
+        HomeCateegoryItem7.add(new ChildRecyclerViewDataClass(3,"Gravity","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Documentry%20%20movies%2Fgravity.jpg?alt=media&token=279f707b-ba1b-49db-8b79-ec0757593212",""));
+        HomeCateegoryItem7.add(new ChildRecyclerViewDataClass(4,"Senna","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Documentry%20%20movies%2FSenna.jpg?alt=media&token=05a751ae-4764-4d18-b62d-c8c419227d71",""));
+        HomeCateegoryItem7.add(new ChildRecyclerViewDataClass(5,"Borat","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Documentry%20%20movies%2FBorat.jpg?alt=media&token=a73af302-6553-4323-bc85-9be2a27b2d56",""));
+
+        //Up Coming Movies
+        List<ChildRecyclerViewDataClass>HomeCateegoryItem8 =new ArrayList<>();
+        HomeCateegoryItem8.add(new ChildRecyclerViewDataClass(1,"Civil War","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Up%20%20Coming%20Movies%2FCivil%20War.jpg?alt=media&token=a5452176-5c64-455a-bf8f-5b4b4a05fabb",""));
+        HomeCateegoryItem8.add(new ChildRecyclerViewDataClass(2,"Kalki 2898-AD","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Up%20%20Coming%20Movies%2FKalki%202898.jpg?alt=media&token=4e371016-597d-4461-95cc-c618bd62e0f0",""));
+        HomeCateegoryItem8.add(new ChildRecyclerViewDataClass(3,"Kingdom of the planet of the Apes","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Up%20%20Coming%20Movies%2FKingdom%20of%20the%20planet%20of%20%20the%20apes.jpeg?alt=media&token=49e4562b-c963-4d09-9b65-79e261668158",""));
+        HomeCateegoryItem8.add(new ChildRecyclerViewDataClass(4,"The idea of you","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Up%20%20Coming%20Movies%2FThe%20idea%20of%20the%20you.jpg?alt=media&token=3a877f24-a24a-4227-9e97-71a097a17f61",""));
+        HomeCateegoryItem8.add(new ChildRecyclerViewDataClass(5,"Kung Fu Panda 4","https://firebasestorage.googleapis.com/v0/b/fir-project-de05e.appspot.com/o/Up%20%20Coming%20Movies%2Fkung%20fu%20panda.jpg?alt=media&token=e9215b80-f2c0-44ec-9014-9e6a3594afa7",""));
+
 
         allcategoryList =new ArrayList<>();
         allcategoryList.add(new ParentRecyclerViewData(1,"Trending Movies",HomeCateegoryItem1));
-        allcategoryList.add(new ParentRecyclerViewData(1,"Movies in Hindi",HomeCateegoryItem2));
-        allcategoryList.add(new ParentRecyclerViewData(1,"Kids and Family Movies",HomeCateegoryItem3));
-        allcategoryList.add(new ParentRecyclerViewData(1,"Up Coming Movies",HomeCateegoryItem4));
+        allcategoryList.add(new ParentRecyclerViewData(2,"Movies in Hindi",HomeCateegoryItem2));
+        allcategoryList.add(new ParentRecyclerViewData(3,"Kids and Family Movies",HomeCateegoryItem3));
+        allcategoryList.add(new ParentRecyclerViewData(4,"Romantic Movies",HomeCateegoryItem4));
+        allcategoryList.add(new ParentRecyclerViewData(5,"Action and Thriller",HomeCateegoryItem5));
+        allcategoryList.add(new ParentRecyclerViewData(6,"Top-rated IMDb movies",HomeCateegoryItem6));
+        allcategoryList.add(new ParentRecyclerViewData(7,"Documentries",HomeCateegoryItem7));
+        allcategoryList.add(new ParentRecyclerViewData(8,"Up Coming Movies",HomeCateegoryItem8));
+         
+
 
         setParentRecyclerView(allcategoryList);
     }
